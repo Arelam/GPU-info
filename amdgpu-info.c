5acb5cd9a05f52fa5621c8755678b21e7152bdbe
Are Kristoffer Lamo <arelamo@studioare.net>
2020-09-03 12:53:14 +0200

    Organized functions.

--- amdgpu-info.c
+++ amdgpu-info.c
@@ -15,103 +15,105 @@ void intHandler(int dummy) {
     keepRunning = 0;
 }
 
+// https://stackoverflow.com/questions/2693776/removing-trailing-newline-character-from-fgets-input
+char *removeNewline(char *string)
+{
+	string[strcspn(string, "\r\n")] = 0; // works for LF, CR, CRLF, LFCR
+	return string;
+}
+
+char *readFile(const char *file)
+    {
+    //https://stackoverflow.com/a/14424800
+    
+    int length=0;
+    FILE *fp;
+    fp = fopen(file, "r");
+    if(!fp)
+    {
+        return NULL;
+    }
+    fseek(fp, 0, SEEK_END);
+    length=ftell(fp);
+    rewind(fp);
+    
+    char *res = (char *) malloc(length);
+    fread(res, 1, length, fp);
+    //fgets(tmp, sizeof(tmp), fp);
+    fclose(fp);
+
+    /*if (length > 0 && res[length-1] == '\n') {
+        res[--length] = '\0';
+    }*/
+
+    return res;
+}
+
+/* WATTS */
+int getWatts() 
+{
+	int watt = atoi(readFile(SENSORFILE(SYSDIR, "power1_average")));
+    return watt/1000000;
+}
+int getWattCap() 
+{
+    int watt = atoi(readFile(SENSORFILE(SYSDIR, "power1_cap")));
+    return watt/1000000;
+}
+
+/* TEMP */
+int getTemp() 
+{
+    int temp = atoi(readFile(SENSORFILE(SYSDIR, "temp1_input")));
+    return temp/1000;
+}
+int getTempCrit() 
+{
+    int temp = atoi(readFile(SENSORFILE(SYSDIR, "temp1_crit")));
+    return temp/1000000;
+}
+
+/* FAN */
+int getFanRPM() 
+{
+    return atoi(readFile(SENSORFILE(SYSDIR, "fan1_input")));
+}  
+int getFanPWM() 
+{
+    return atoi(readFile(SENSORFILE(SYSDIR, "pwm1")));
+}
+int getFanPWMMAX() 
+{
+    return atoi(readFile(SENSORFILE(SYSDIR, "pwm1_max")));
+}
+
+/* VOLTAGE */
+char *getVoltageType() 
+{
+    return removeNewline(readFile(SENSORFILE(SYSDIR, "in0_label")));
+}
+int getVoltage() 
+{
+    return atoi(readFile(SENSORFILE(SYSDIR, "in0_input")));
+}
+
+// TODO memory and clock
+/* MEMORY */
+char *getMemoryStates() 
+{
+    return readFile("/sys/class/drm/card0/device/pp_dpm_mclk");
+}
+
+// /sys/class/drm/card0/device/pp_dpm_sclk
+// 0: 300Mhz *
+// 1: 483Mhz 
+// 2....
+	
 int main()
 {
     signal(SIGINT, intHandler);
-    
     // http://rosettacode.org/wiki/Terminal_control/Preserve_screen#C
     printf("\033[?1049h\033[H");
-    char *readFile(const char *file)
-    {
-        //https://stackoverflow.com/a/14424800
-        
-        int length=0;
-        FILE *fp;
-        fp = fopen(file, "r");
-        if(!fp)
-        {
-            return NULL;
-        }
-        fseek(fp, 0, SEEK_END);
-        length=ftell(fp);
-        rewind(fp);
-        
-        char *res = (char *) malloc(length);
-        fread(res, 1, length, fp);
-        //fgets(tmp, sizeof(tmp), fp);
-        fclose(fp);
-
-        /*if (length > 0 && res[length-1] == '\n') {
-            res[--length] = '\0';
-        }*/
-  
-        return res;
-    }
-    char *removeNewline(char *string)
-    {
-        // https://stackoverflow.com/questions/2693776/removing-trailing-newline-character-from-fgets-input
-        string[strcspn(string, "\r\n")] = 0; // works for LF, CR, CRLF, LFCR
-        return string;
-    }
-    /* WATTS */
-    int getWatts() 
-    {
-    	int watt = atoi(readFile(SENSORFILE(SYSDIR, "power1_average")));
-        //int watt = atoi(readFile("/sys/class/drm/card0/device/hwmon/hwmon1/power1_average"));
-        return watt/1000000;
-    }
-    int getWattCap() 
-    {
-        int watt = atoi(readFile(SENSORFILE(SYSDIR, "power1_cap")));
-        return watt/1000000;
-    }
-    
-    /* TEMP */
-    int getTemp() 
-    {
-        int temp = atoi(readFile(SENSORFILE(SYSDIR, "temp1_input")));
-        return temp/1000;
-    }
-    int getTempCrit() 
-    {
-        int temp = atoi(readFile(SENSORFILE(SYSDIR, "temp1_crit")));
-        return temp/1000000;
-    }
-    
-    /* FAN */
-    int getFanRPM() 
-    {
-        return atoi(readFile(SENSORFILE(SYSDIR, "fan1_input")));
-    }  
-    int getFanPWM() 
-    {
-        return atoi(readFile(SENSORFILE(SYSDIR, "pwm1")));
-    }
-    int getFanPWMMAX() 
-    {
-        return atoi(readFile(SENSORFILE(SYSDIR, "pwm1_max")));
-    }
-    
-    /* VOLTAGE */
-    char *getVoltageType() 
-    {
-        return removeNewline(readFile(SENSORFILE(SYSDIR, "in0_label")));
-    }
-    int getVoltage() 
-    {
-        return atoi(readFile(SENSORFILE(SYSDIR, "in0_input")));
-    }
-    
-    /* MEMORY */
-    char *getMemoryStates() 
-    {
-        return readFile("/sys/class/drm/card0/device/pp_dpm_mclk");
-    }
-    
-    // /sys/class/drm/card0/device/pp_dpm_sclk
-    // 0: 300Mhz *
-	// 1: 483Mhz 
-	// 2....
 
     while(keepRunning) {
         // TODO Flags table https://www.eecs.wsu.edu/~cs150/reading/printf.htm
@@ -130,7 +132,7 @@ int main()
         // 11 columns space width
         printf("%-11.11s %imV\n", getVoltageType(), getVoltage());
         
-        printf("%-11.11s %iW (%i%% of %iW limit)\n", "POWER", watt, wattPercent, wattCap);
+        printf("%-11.11s %iW (%i%% of %iW\n", "POWER", watt, wattPercent, wattCap);
         
         printf("%-11.11s %i\u2103  (critical: %i\u00B0C)\n", "Temperature", getTemp(), getTempCrit());
         
